610c33c37d2a3c84f9d615f90c32ef6e3f012139
01011888332 <>
2023-12-11 20:06:03 +0200

    signupform

--- gmail/src/router/index.js
+++ gmail/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import signUpForm from '../views/signUpForm.vue'
+import loginForm from '../views/loginForm.vue'
 const routes = [
   {
     path: '/',
@@ -8,12 +9,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/signup',
+    name: 'signup',
+    component: signUpForm
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: loginForm
   }
 ]
 
---
+++ gmail/src/views/loginForm.vue
@@ -0,0 +1,292 @@
+<template>
+  <div class="all">
+    <div class="container">
+      <section>
+        <div class="img">
+        </div>
+        <div class="form">
+          <form @submit.prevent="logIn">
+            <router-link to="/">
+              <h2>
+                <i
+                  class="fa-solid fa-arrow-left"
+                  style="color: #000000"
+                ></i></h2
+            ></router-link>
+            <h3>Hi Again!</h3>
+            <div
+              v-show="this.missingData"
+              class="alert alert-danger"
+              role="alert"
+            >
+              <h5>Some Data Is Missing</h5>
+            </div>
+            <div class="row">
+              <div class="col">
+                <label>Email</label>
+                <input type="email" required v-model="email" />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <label>Password</label>
+                <input type="password" required v-model="password" />
+                <div
+                  v-show="this.userNotFound"
+                  class="alert alert-danger"
+                  role="alert"
+                >
+                  <h5>Wrong Email Or Paassword</h5>
+                </div>
+              </div>
+              <router-link to="/forgetPassword" class="nav-link links">
+                <h6>Forget Your Password</h6>
+              </router-link>
+            </div>
+            <div class="row">
+              <button @click="checkMissingData" class="butn">Login</button>
+            </div>
+
+            <div class="row">
+              <router-link to="/signup" class="nav-link links">
+                <h6 class="center">Don't have an account? Sign up</h6>
+              </router-link>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import $store from "../store/index.js";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      userNotFound: false,
+      missingData: false,
+    };
+  },
+  created() {
+    this.checkLoggedIn();
+  },
+  methods: {
+    logIn(event) {
+      this.checkMissingData();
+      this.userNotFound = false;
+      const email = this.email;
+      const password = this.password;
+      const apiUrl = `http://localhost:8080/checkPass/${email}`;
+
+      fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(password),
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else if (response.status === 404) {
+            return null;
+          } else {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+        })
+        .then((userData) => {
+          if (userData !== null) {
+            console.log("Login successful");
+            const fakeToken = "your-fake-token";
+            const userDataString = JSON.stringify(userData);
+            localStorage.setItem("userData", userDataString);
+            localStorage.setItem("token", fakeToken);
+            $store.commit("setLoginStatus", true);
+            $store.commit("setCurrUser", userData);
+            this.$router.push({ name: "home" });
+          } else {
+            this.userNotFound = true;
+          }
+        })
+        .catch((error) => {
+          console.error("Error during login:", error);
+        });
+
+      // fetch(`http://localhost:8080/users/${email}`)
+      //   .then((res) => {
+      //     if (res.ok) {
+      //       return res.json();
+      //     } else if (res.status === 404) {
+      //       throw new Error("User not found");
+      //     } else {
+      //       throw new Error(`Error: ${res.status}`);
+      //     }
+      //   })
+      //   .then((user) => {
+      //     console.log(user);
+      //     if (user.password === password) {
+      //       const fakeToken = "your-fake-token";
+      //       localStorage.setItem("token", fakeToken);
+      //       storeID.currUser = user;
+      //       this.$emit("changeState", "");
+      //       this.$emit("login", true);
+      //     } else {
+      //       this.userNotFound = true;
+      //       event.preventDefault();
+      //     }
+      //   })
+      //   .catch((error) => {
+      //     console.error("Error during login:", error);
+      //   });
+    },
+    checkLoggedIn() {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const userDataString = localStorage.getItem("userData");
+        const userData = JSON.parse(userDataString);
+      }
+    },
+    checkMissingData() {
+      if (this.email.length == 0 || this.password.length == 0) {
+        this.missingData = true;
+      } else {
+        this.missingData = false;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  text-align: center;
+}
+.img {
+  position: relative;
+  width: 55%;
+  top: -130px;
+}
+
+input {
+  margin-right: 5px;
+  display: block;
+  padding: 4px 2px;
+  width: 100%;
+  border: none;
+  border-bottom: 2px solid #ddd;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+h2 {
+  text-align: start;
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+.form {
+  position: absolute;
+  border-radius: 40px;
+  padding: 60px 80px 40px 80px;
+  width: 400px;
+  background-color: #e0dbdb;
+  text-align: start;
+  right: 5%;
+  border-bottom: 5px solid #9c9c9c;
+  top: 20px;
+}
+.butn {
+  background-color: #bf7934;
+  border-radius: 50px;
+  margin-top: 30px;
+  padding: 8px;
+  border: 0px solid transparent;
+}
+label {
+  margin: 25px 0px 15px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+section {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 600px;
+}
+
+h6 {
+  direction: rtl;
+  cursor: pointer;
+  padding: 20px;
+  text-decoration: underline;
+}
+h4 {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 14px;
+  color: #000;
+  position: relative;
+  display: inline-block;
+}
+h4::before,
+h4::after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  width: 50%;
+  height: 1px;
+  background-color: black;
+}
+
+h4::before {
+  left: 0;
+  transform: translateX(-40%);
+}
+
+h4::after {
+  right: 0;
+  transform: translateX(40%);
+}
+
+.icons {
+  margin-top: 20px;
+}
+.shortcut {
+  background-color: #fff;
+  border: 1px solid #000;
+  border-radius: 50%;
+  cursor: pointer;
+  border: none;
+}
+.center {
+  margin: 0 auto;
+  cursor: pointer;
+  text-align: center;
+}
+.alert {
+  margin-top: 10px;
+  padding: 7px 10px;
+}
+h5 {
+  color: red;
+  font-size: 14px;
+  margin-top: 5px;
+}
+a {
+  font-weight: bold;
+  color: black;
+  text-decoration: none;
+}
+
+a.router-link-exact-active {
+  color: black;
+  text-decoration: none;
+}
+</style>
